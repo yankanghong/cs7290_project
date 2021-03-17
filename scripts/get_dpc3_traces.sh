e589c7a715ef9aab79e214b2726b556eeadc056b
Kanghong Yan <shayneyan@outlook.com>
2021-03-16 22:51:54 -0400

    update scripts

--- scripts/get_dpc3_traces.sh
+++ scripts/get_dpc3_traces.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ## download traces based on spec2017_simpoint.txt
-mkdir -p $PWD/../dpc3_traces
+mkdir -p $PWD/../traces/dpc3_traces
 while read LINE
 do
-    wget -P $PWD/../dpc3_traces -c http://hpca23.cse.tamu.edu/champsim-traces/speccpu/$LINE
+    wget -P $PWD/../traces/dpc3_traces -c http://hpca23.cse.tamu.edu/champsim-traces/speccpu/$LINE
 done < spec2017_simpoint.txt
