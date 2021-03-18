af0af979212efa6d2efb4656f8ce380397ce9c4f
Kanghong Yan <kyan41@login-coc-ice-1.pace.gatech.edu>
2021-03-18 15:34:07 -0400

    small update to scripts

--- scripts/get_dpc3_traces.sh
+++ scripts/get_dpc3_traces.sh
@@ -3,5 +3,8 @@
 mkdir -p $PWD/../traces/dpc3_traces
 while read LINE
 do
-    wget -P $PWD/../traces/dpc3_traces -c http://hpca23.cse.tamu.edu/champsim-traces/speccpu/$LINE
+    if [[ ${LINE:0:1} != '#' ]]  
+    then
+        wget -P $PWD/../traces/dpc3_traces -c http://hpca23.cse.tamu.edu/champsim-traces/speccpu/$LINE
+    fi
 done < spec2017_simpoint.txt
